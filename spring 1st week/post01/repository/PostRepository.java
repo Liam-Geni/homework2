c669bfc3b3337a151e402ca310a331620319a734
Liam-Genius <66250121+Liam-Genius@users.noreply.github.com>
2022-10-07 03:37:49 +0900

    Delete spring 1st week/post01/repository directory

--- spring 1st week/post01/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.post01.repository;
-
-import com.sparta.post01.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
-
